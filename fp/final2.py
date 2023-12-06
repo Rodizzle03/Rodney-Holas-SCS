9439d4011edf1395307395ab9fdc48322aec6c74
Rodizzle03 <m262832@usna.edu>
2023-12-06 18:25:49 -0500

    More updates

--- fp/final2.py
+++ fp/final2.py
@@ -27,6 +27,7 @@ bulleting = pygame.image.load('../fp/assets/sprites/bullet.png')
 # Fonts
 font = pygame.font.SysFont('Arial', 32, 'bold')
 font_gameover = pygame.font.SysFont('Arial', 64, 'bold')
+font_countdown = pygame.font.SysFont('Arial', 32, 'bold')
 
 # Sounds
 laser_sound = mixer.Sound('../fp/assets/sounds/lazer.mp3')
@@ -41,16 +42,16 @@ spaceship2X = 370
 spaceship2Y = 480
 change2X = 0
 
-num_of_aliens = 12
+num_of_aliens = 6
 alienX = [random.randint(0, 736) for _ in range(num_of_aliens)]
 alienY = [random.randint(30, 150) for _ in range(num_of_aliens)]
-alienspeedX = [-0.25 for _ in range(num_of_aliens)]
-alienspeedY = [30 for _ in range(num_of_aliens)]
+alienspeedX = [-2 for _ in range(num_of_aliens)]
+alienspeedY = [25 for _ in range(num_of_aliens)]
 
 alien2X = [random.randint(0, 736) for _ in range(num_of_aliens)]
 alien2Y = [random.randint(20, 150) for _ in range(num_of_aliens)]
-alienspeed2X = [1 for _ in range(num_of_aliens)]
-alienspeed2Y = [20 for _ in range(num_of_aliens)]
+alienspeed2X = [-2 for _ in range(num_of_aliens)]
+alienspeed2Y = [25 for _ in range(num_of_aliens)]
 
 bulletX = 386
 bulletY = 450
@@ -67,6 +68,12 @@ level = 1  # Initial level
 max_score_per_level = 20  # Number of points needed to advance to the next level
 winning_score = 100  # Number of points needed to win the game
 
+# Add time-related variables
+start_time = 0
+time_limit_seconds = 60  # Set the time limit in seconds
+
+fastest_winning_time = float('inf')  # Initialize with positive infinity
+
 running = True
 game_active = False  # Variable to control if the game is active or in the menu
 num_players = 1  # Default number of players
@@ -81,20 +88,58 @@ def score_text():
     screen.blit(img, (10, 10))
 
 
+def draw_reset_screen():
+    screen.fill((0, 0, 0))  # Fill the screen with black
+    menu_font = pygame.font.SysFont('Arial', 64, 'bold')
+    title_text = menu_font.render('Space Cowboy Shooter', True, 'white')
+    start_text = font.render('Press 1 for 1 Player\nPress 2 for 2 Players\nPress SPACE to start', True, 'white')
+
+    screen.blit(title_text, (150, 200))
+    screen.blit(start_text, (50, 350))
+
+
 def gameover():
     img_gm = font_gameover.render('GAME OVER', True, 'white')
     screen.blit(img_gm, (200, 250))
 
 
 def winning_screen():
+    global fastest_winning_time
     img_win = font_gameover.render('YOU WIN!', True, 'white')
     screen.blit(img_win, (250, 250))
 
+    # Update the fastest winning time if the current time is faster
+    current_time = pygame.time.get_ticks()
+    elapsed_time = (current_time - start_time) // 1000
+    if elapsed_time < fastest_winning_time:
+        fastest_winning_time = elapsed_time
+
+
+# New function to draw the "Next Level" screen
+def draw_next_level_screen():
+    screen.fill((0, 0, 0))  # Fill the screen with black
+    menu_font = pygame.font.SysFont('Arial', 64, 'bold')
+    title_text = menu_font.render('Next Level', True, 'white')
+    press_space_text = font.render('Press R to continue', True, 'white')
+
+    # Adjust the coordinates to center the text
+    title_text_rect = title_text.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2 - 50))
+    press_space_text_rect = press_space_text.get_rect(center=(screen.get_width() // 2, screen.get_height() // 2 + 50))
+
+    screen.blit(title_text, title_text_rect)
+    screen.blit(press_space_text, press_space_text_rect)
+
 
 def main_menu():
+    screen.fill((0, 0, 0))  # Fill the screen with black
+
     menu_font = pygame.font.SysFont('Arial', 64, 'bold')
     title_text = menu_font.render('Space Cowboy Shooter', True, 'white')
     start_text = font.render('Press 1 for 1 Player\nPress 2 for 2 Players\nPress SPACE to start', True, 'white')
+
+    # Display the fastest winning time on the main menu
+    fastest_time_text = font.render(f'Fastest Winning Time: {fastest_winning_time}s', True, 'white')
+    screen.blit(fastest_time_text, (10, 550))
 
     screen.blit(title_text, (150, 200))
     screen.blit(start_text, (50, 350))
@@ -106,6 +151,10 @@ def reset_game():
     global spaceshipX, spaceshipY, spaceship2X, spaceship2Y
     global bulletX, bulletY, check, bullet2X, bullet2Y, check2
     global score, score2, level, game_active
+
+    mixer.music.stop()  # Stop the music
+    mixer.music.load('../fp/assets/sounds/Halo.mp3')  # Load the music again
+    mixer.music.play(-1)  # Start playing the music from the beginning
 
     spaceshipX = 370
     spaceshipY = 480
@@ -132,25 +181,43 @@ def reset_game():
 
 # Main game loop
 while running:
+    current_time = pygame.time.get_ticks()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        if not game_active:  # If the game is not active, check for key input in the menu
+        if not game_active:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
                     num_players = 1
+                    start_time = current_time
                 elif event.key == pygame.K_2:
                     num_players = 2
+                    start_time = current_time
                 elif event.key == pygame.K_SPACE:
+                    draw_next_level_screen()
+                    pygame.display.update()
+                    pygame.time.delay(2000)
                     game_active = True
+                    start_time = current_time
 
     if game_active:
+        elapsed_time = (current_time - start_time) // 1000
+        remaining_time = max(0, time_limit_seconds - elapsed_time)
+
+        if elapsed_time >= time_limit_seconds:
+            game_active = False
+            gameover()
+            pygame.display.update()
+            pygame.time.delay(2000)
+            reset_game()
+            draw_reset_screen()
+
         if game_active:
             screen.blit(background, (0, 0))
 
             keys = pygame.key.get_pressed()
-
             # Player 1 controls
             if keys[pygame.K_a]:
                 spaceshipX -= 0.5
@@ -204,9 +271,11 @@ while running:
 
             alienX[i] += alienspeedX[i]
             if alienX[i] <= 0:
+                # noinspection PyTypeChecker
                 alienspeedX[i] = 0.25
                 alienY[i] += alienspeedY[i]
             if alienX[i] >= 736:
+                # noinspection PyTypeChecker
                 alienspeedX[i] = -0.25
                 alienY[i] += alienspeedY[i]
 
@@ -224,19 +293,40 @@ while running:
                     winning_screen()
                     winner = mixer.Sound('../fp/assets/sounds/win.mp3')
                     winner.play()
+
+                    # Update the fastest winning time if the current time is faster
+                    if elapsed_time < fastest_winning_time:
+                        fastest_winning_time = elapsed_time
+
                     reset_game()
+                    draw_reset_screen()  # Call the new function to draw the reset screen
 
                 # Check if the player has reached the required score for the next level
-                elif score >= level * max_score_per_level:
+                if score >= level * max_score_per_level or score2 >= level * max_score_per_level:
                     level += 1
+                    draw_next_level_screen()
+                    pygame.display.update()
+
+                    # Wait for a key press before proceeding to the next level
+                    wait_for_key = True
+                    while wait_for_key:
+                        for event in pygame.event.get():
+                            if event.type == pygame.QUIT:
+                                running = False
+                                wait_for_key = False
+                            elif event.type == pygame.KEYDOWN:
+                                wait_for_key = False
+
                     # Increase the difficulty by modifying game variables
                     # You can adjust these values to make the game harder
                     alienspeedX = [-1 * level for _ in range(num_of_aliens)]
                     alienspeedY = [2 * level for _ in range(num_of_aliens)]
+                    alienspeed2X = [1 * level for _ in range(num_of_aliens)]
 
             screen.blit(aliening, (alienX[i], alienY[i]))
 
             if num_players == 2:
+                num_of_aliens = 12
                 if alien2Y[i] > 420:
                     for j in range(num_of_aliens):
                         alien2Y[j] = 2000
@@ -245,9 +335,11 @@ while running:
 
                 alien2X[i] += alienspeed2X[i]
                 if alien2X[i] <= 0:
+                    # noinspection PyTypeChecker
                     alienspeed2X[i] = 0.25
                     alien2Y[i] += alienspeed2Y[i]
                 if alien2X[i] >= 736:
+                    # noinspection PyTypeChecker
                     alienspeed2X[i] = -0.25
                     alien2Y[i] += alienspeed2Y[i]
 
@@ -262,17 +354,37 @@ while running:
 
                     # Check if the player has won
                     if score2 >= winning_score:
-                        game_active = False
                         winning_screen()
                         winner = mixer.Sound('../fp/assets/sounds/win.mp3')
                         winner.play()
+
+                        # Update the fastest winning time if the current time is faster
+                        if elapsed_time < fastest_winning_time:
+                            fastest_winning_time = elapsed_time
+
                         reset_game()
+                        draw_reset_screen()  # Call the new function to draw the reset screen
 
                     # Check if the player has reached the required score for the next level
-                    elif score2 >= level * max_score_per_level:
+                    if score >= level * max_score_per_level or score2 >= level * max_score_per_level:
                         level += 1
+                        draw_next_level_screen()
+                        pygame.display.update()
+
+                        # Wait for a key press before proceeding to the next level
+                        wait_for_key = True
+                        while wait_for_key:
+                            for event in pygame.event.get():
+                                if event.type == pygame.QUIT:
+                                    running = False
+                                    wait_for_key = False
+                                elif event.type == pygame.KEYDOWN:
+                                    wait_for_key = False
+
                         # Increase the difficulty by modifying game variables
                         # You can adjust these values to make the game harder
+                        alienspeedX = [-1 * level for _ in range(num_of_aliens)]
+                        alienspeedY = [2 * level for _ in range(num_of_aliens)]
                         alienspeed2X = [1 * level for _ in range(num_of_aliens)]
                         alienspeed2Y = [5 * level for _ in range(num_of_aliens)]
 
@@ -297,7 +409,19 @@ while running:
         if num_players == 2:
             screen.blit(spaceshiping2, (spaceship2X, spaceship2Y))  # Draw second spaceship
         score_text()
+
+        # Draw countdown timer in the upper right-hand corner
+        countdown_text = font_countdown.render(f'Time: {remaining_time}s', True, 'white')
+        screen.blit(countdown_text, (screen.get_width() - countdown_text.get_width() - 10, 10))
+
     else:
+
+        # Draw the fastest winning time on the main menu screen
+
         main_menu()
+
+        fastest_time_text = font.render(f'Fastest Winning Time: {fastest_winning_time}s', True, 'white')
+
+        screen.blit(fastest_time_text, (150, 450))
 
     pygame.display.update()
